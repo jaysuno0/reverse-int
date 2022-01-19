e83981bf01f5398a2faa512cc0f412984080d237
Daniil Kochkin <jaysun0@yandex.ru>
2022-01-19 05:05:37 +0300

    feat: add function to reverse given number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(number) {
+  const regularExp = /[0-9]/g;
+  const numberArray = number.toString().match(regularExp);
+  const strReversedNumber = numberArray.reverse().join('');
+  return +strReversedNumber;
 }
